30f63c8fb9779d4baa311d0e68b9138d017a0f63
Derek Jackson <d4jackson84@gmail.com>
2021-12-18 17:15:53 -0500

    initialized backend on firebase

--- backend/functions/src/db.ts
+++ backend/functions/src/db.ts
@@ -1,7 +1,11 @@
 import {MongoClient} from "mongodb";
-import * as functions from "firebase-functions";
+// import * as functions from "firebase-functions";
 
-const uri = functions.config().mongodb.uri || "";
+// const uri = functions.config().mongodb.uri || "";
+const functions = require("firebase-functions");
+// const { MongoClient } = require("mongodb");
+
+const uri: string = functions.config().mongodb.uri;
 
 if (!uri){
     console.error("Error: Missing environment variable MONGO_URI");
